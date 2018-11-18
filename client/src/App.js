ec953efcdf68376c5c7f585be6fd304125c655f5
Justin Mason <justinmason@172-18-45-35.DYNAPOOL.NYU.EDU>
2018-11-17 22:49:50 -0500

    Refactor textarea placeholders; ensure output_box will update when translation received

--- client/src/App.js
+++ client/src/App.js
@@ -66,9 +66,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <textarea id="input_box" name="input_box" rows="10" cols="90" placeholder="Input Text" onChange={this.updateInput}></textarea>
+        <textarea id="input_box" name="input_box" rows="10" cols="90" placeholder="Enter text here" onChange={this.updateInput}></textarea>
         <input type="submit" value="Translate to English" onClick={this.translate}/>
-        <textarea id="output_box" name="output_box" rows="10" cols="90" placeholder="Translation">{this.state.translation}</textarea>
+        <textarea id="output_box" name="output_box" rows="10" cols="90" placeholder="Translation will appear here" value={this.state.translation}/>
         <p style={{display:this.state.languageDisplay}}>(translated from {this.state.language})</p>
       </div>
     );
