6a1bd7b7e992158e6e7dca2383f597262ab2299f
Justin Mason <justinmason@172-18-45-35.DYNAPOOL.NYU.EDU>
2018-11-17 23:31:06 -0500

    Add translation functionality to application

--- server.js
+++ server.js
@@ -5,12 +5,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const {Translate} = require('@google-cloud/translate');
+
+if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  // To obtain credentials, go to https://cloud.google.com/translate/docs/quickstart
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = "ENTER THE PATH TO YOUR GOOGLE APPLICATION CREDENTIALS HERE";
+}
+
+const projectId = 'translator-222623'; // Change this to the projectId listed in your credentials json file
+const translate = new Translate({projectId: projectId});
+
 app.post('/detectLanguage', (req, res) => {
-  res.send("Langauge!");
+  const input = req.body.input;
+
+  translate.detect(input).then(languages => {
+    res.send(languages[0].language);
+  }).catch(err => {
+    res.send("ERROR!");
+  });
 });
 
 app.post('/translate', (req, res) => {
-  res.send("Translation!");
+  const input = req.body.input;
+
+  translate.translate(input, "en").then(translations => {
+    res.send(translations[0]);
+  }).catch(err => {
+      res.send("ERROR!");
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
